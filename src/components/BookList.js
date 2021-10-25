ca5aa151649ab249196b5a4049f46f86156aa671
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-25 13:11:59 +0100

    Access data from components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ color: theme.syntax, background: theme.bg }}
+      >
         <ul>
-          <li>The lord of the rings</li>
-          <li>Harry Potter</li>
-          <li>The neverending story</li>
+          <li style={{ background: theme.ui }}>The lord of the rings</li>
+          <li style={{ background: theme.ui }}>Harry Potter</li>
+          <li style={{ background: theme.ui }}>The neverending story</li>
         </ul>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
+  // Look up the component tree to the provider for this context, to have access to the data
+  static contextType = ThemeContext;
   render() {
+    // Extract context data into constants
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Context App</h1>
         <ul>
           <li>Home</li>
