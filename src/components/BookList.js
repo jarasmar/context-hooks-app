d0b90be3ff08d6564df06ee1f8002624d4239fb1
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-25 13:33:24 +0100

    Update context to use consumers to allow for functional components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
-  static contextType = ThemeContext;
   render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>The lord of the rings</li>
-          <li style={{ background: theme.ui }}>Harry Potter</li>
-          <li style={{ background: theme.ui }}>The neverending story</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, light, dark } = this.context;
+          const theme = isLightTheme ? light : dark;
+          <div
+            className="book-list"
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
+            <ul>
+              <li style={{ background: theme.ui }}>The lord of the rings</li>
+              <li style={{ background: theme.ui }}>Harry Potter</li>
+              <li style={{ background: theme.ui }}>The neverending story</li>
+            </ul>
+          </div>;
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,24 @@ import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
-  // Look up the component tree to the provider for this context, to have access to the data
-  static contextType = ThemeContext;
   render() {
-    // Extract context data into constants
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.syntax }}>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      //Add a Consumer to Look up the component tree to the provider for this context and give have access to the data
+      <ThemeContext.Consumer>
+        {(context) => {
+          // Extract context data into constants
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          <nav style={{ background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>;
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
