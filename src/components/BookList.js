195d8f1912c58476953b57746143bfac54e6e2ef
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-26 08:22:31 +0100

    Use context with a hook in functional components

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,29 +1,22 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div
-              className="book-list"
-              style={{ color: theme.syntax, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>The lord of the rings</li>
-                <li style={{ background: theme.ui }}>Harry Potter</li>
-                <li style={{ background: theme.ui }}>The neverending story</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const BookList = () => {
+  // Using context from a hook for functional components (cleaner than a Consumer)
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>The lord of the rings</li>
+        <li style={{ background: theme.ui }}>Harry Potter</li>
+        <li style={{ background: theme.ui }}>The neverending story</li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
